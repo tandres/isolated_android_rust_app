0762546172fd91ce700ac545a342aa573fd4767f
Trenton Andres <trenton.andres@gmail.com>
2022-01-16 12:15:52 -0600

    rust: checkin of basic helloworld code

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,17 @@
+use std::os::raw::{c_char};
+use std::ffi::{CString, CStr};
+
+#[no_mangle]
+fn rust_hello(name: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(name) };
+    let rname = match c_str.to_str() {
+        Ok(s) => s,
+        Err(_) => "unknown",
+    };
+    let rstr = format!("Hello, {}", rname);
+    CString::new(rstr).unwrap().into_raw()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
