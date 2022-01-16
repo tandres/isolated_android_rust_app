f70894e9446621fe0372ac746e9f090a796c5703
Trenton Andres <trenton.andres@gmail.com>
2022-01-16 15:38:36 -0600

    app: reading from local file

--- app/app/src/main/java/com/tandres/isolatedrustapp/MainActivity.java
+++ app/app/src/main/java/com/tandres/isolatedrustapp/MainActivity.java
@@ -3,10 +3,16 @@ package com.tandres.isolatedrustapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "IsolatedMain";
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, "Starting");
         RustHelloWorld.main("TJ");
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    try {
+                        FileInputStream fis = new FileInputStream(new File( getFilesDir(),"test_file"));
+                        byte[] buf = new byte[100];
+                        fis.read(buf);
+                        Log.i(TAG, "BUF IS: " + new String(buf));
+                    }catch (Exception e) {
+                        Log.e(TAG, e.toString());
+                    }
+
+                    //String res = RustHelloWorld.read_file(fis.getFD());
+                    /*if (res != null) {
+                        Log.i(TAG, "OUTPUT RESULT: " + res);
+                    }*/
+                } catch(Exception e) {
+                    Log.e(TAG, "Got exception " + e);
+                }
+
+            }
+        }, 10000);
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,16 +1,29 @@
+use std::{
+    io::Read,
+    os::unix::prelude::FromRawFd,
+};
+
 use jni::JNIEnv;
-
 use jni::objects::{JClass, JString};
-
-use jni::sys::jstring;
+use jni::sys::{jint, jstring};
 
 #[no_mangle]
-pub extern "system" fn Java_com_tandres_isolatedrustapp_RustHelloWorld_hello(env: JNIEnv, class: JClass, input: JString) -> jstring {
+pub extern "system" fn Java_com_tandres_isolatedrustapp_RustHelloWorld_hello(env: JNIEnv, _class: JClass, input: JString) -> jstring {
     let input: String = env.get_string(input).expect("Couldn't get java string!").into();
 
     let output = env.new_string(format!("Hello, {}!", input)).expect("Couldn't create java string!");
 
     output.into_inner()
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_tandres_isolatedrustapp_RustHelloWorld_read_file(env: JNIEnv, _class: JClass, input: jint) -> jstring {
+    let input: i32 = input;
+    let mut buf = String::new();
+    let mut file = unsafe { std::fs::File::from_raw_fd(input) };
+    file.read_to_string(&mut buf).expect("Failed to read from string!");
+
+    env.new_string(buf).expect("Couldn't create java string!").into_inner()
 }
 
 #[cfg(test)]
